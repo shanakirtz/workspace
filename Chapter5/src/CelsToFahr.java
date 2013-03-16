44a724fa1c2cc00432ac32a38c8d844ae44b09dc
shana <shanakirtz@yahoo.com>
2013-03-16 12:09:41 -0400

    chapter 5

---
+++ Chapter5/src/CelsToFahr.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class CelsToFahr {
+
+	public static void main(String[] args) {
+	
+	double c;
+	double f;
+	
+		for(c=0; c <= 20; c++)
+			// 9/5 = 1.8
+		{  f = (1.8 * c) + 32;
+		System.out.print("C: "  +  c +  " F:" + f);
+			
+		}
+		
+	}
+
+}
---
+++ Chapter5/src/LargeToSmall.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class LargeToSmall {
+
+	
+	public static void main(String[] args) {
+	 int number;
+	 Scanner scan = new Scanner(System.in);
+	
+	 
+	 System.out.println("Please enter a number.");
+	 System.out.println("To end use -99");
+	 
+	  number = scan.nextInt();
+	 
+	 while (number != -99){
+		 System.out.println("Please enter another number");
+	 number = scan.nextInt();
+	 
+	 }
+	 
+		 
+			 
+	 
+	 
+
+	}
+
+}
